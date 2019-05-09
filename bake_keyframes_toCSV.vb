190e9b117540ca25d250b943e4af94372e9dbcfd
Sanchez <Israel.Sanchez@espn.com>
2019-05-09 14:28:46 -0400

    Updated single container keyframe baker

--- bake_keyframes_toCSV.vb
+++ bake_keyframes_toCSV.vb
@@ -14,10 +14,14 @@ sub OnExecAction(btnId As Integer)
 	Select Case btnId
 	Case 0
 		bakeData()
+	Case 1
+		outputData()
 	End Select
 end sub
 
 
+' EVENT SUBROUTINES ======================================================================
+' ========================================================================================
 Sub bakeData()
 	Dim dir As Director = this.GetDirector()
 	Dim kfArray As Array[Keyframe]
@@ -26,11 +30,26 @@ Sub bakeData()
 	if not(this.IsAnimated()) then println "ERROR:: Container Not Animated"
 	if not(this.IsAnimated()) then exit Sub
 
-	outputDirChArray()
+	'Create full array of all elements in this Stage
+	Dim MainArr As Array[Array[String]] = outputDirChArray()
+
+	'Find sub array of desired channels (this container)
+	Dim chsArr As Array[Array[String]] = chSubArray(MainArr, "#" & CStr(this.vizId))
+
+	'Itterate through all channels and bake keyframes
+	For Each chArr In chsArr
+		if chArr[1].StartsWith("CChannel") then bakeKeyframes(chArr)
+	Next
 End Sub
 
+Sub outputData()
+	'Itterate through all keyframes in all channels and create csv structure
+	'Output csv file to desired folder
+End Sub
 
-Function outputDirChArray() As Array[String]
+' FUNCTIONS ==============================================================================
+' ========================================================================================
+Function outputDirChArray() As Array[Array[String]]
 	Dim x As Integer = -1
 	Dim tempArr, innerArr As Array[String]
 	Dim outArr As Array[Array[String]]
@@ -47,20 +66,70 @@ Function outputDirChArray() As Array[String]
 		end if
 		'ch.AddKeyframe(i/59.94)
 	next
-	for each line in outArr
-		println line[0]
-	next
+' DELETE ::: PRINTS =============
+	'for each line in outArr
+	'	dim str As String = ""
+	'	str.join(line, " - ")
+	'	println str
+	'next
+' ===============================
+	outputDirChArray = outArr
+End Function
 
-	x = 0
-	Dim strArr As Array[String] = outArr[outArr.UBound]
+Function chSubArray(MainArr As Array[Array[String]], vizId As String) As Array[Array[String]]
+	Dim outArr As Array[Array[String]]
+	Dim dirPathStr As String = "---NOPE---"
+	println("=====================================")
+	println("=====================================")
+	println(vizId)
+	For Each line In MainArr
+		IF line[2] = vizId THEN dirPathStr = line[0]
+		If line[0].StartsWith(dirPathStr) Then
+			outArr.Push(line)
+		End If
+	Next
+
+' DELETE ::: PRINTS =============
+	for each line in outArr
+		dim str As String = ""
+		str.join(line, " - ")
+		println str
+	next
+	println("=====================================")
+	for i=0 to 200
+		println i*System.OutputRefreshRate
+	next
+	println("=====================================")
+' ===============================
+
+	chSubArray = outArr
+End Function
+
+
+' SUBROUTINES ============================================================================
+' ========================================================================================
+Sub bakeKeyframes(chArr As Array[String])
+	Dim x As Integer = 0
+	Dim kfIdArr As Array[String]
+	Dim startFrom As Integer = GetParameterInt("startPoint")
+	' ERROR CHECK: Does channel have keyframes?
+	IF CDbl(System.SendCommand(chArr[2] & "*KEYN*1*TIME GET")) = 0 then Exit Sub
+	' Create an array from current keyframe ids
 	Do
-		println system.sendCommand(strArr[2] & "*KEYN*"&x&"*DATA GET")
-		Dim nextKF As String = system.sendCommand(strArr[2] & "*KEYN*"&x&" GET_NEXT_KEYFRAME")
+		kfIdArr.Push(System.SendCommand(chArr[2] & "*KEYN*"&x&"*OBJECT_ID GET"))
+		Dim nextKF As String = System.SendCommand(chArr[2] & "*KEYN*"&x&" GET_NEXT_KEYFRAME")
 		nextKF.trim()
 		if nextKF = "" then Exit Do
+' DELETE ::: FAILSAFE:: ========
+		if x > 20 then Exit Do
+' ==============================
 		x += 1
 	Loop
-End Function
+
+	for each kfId in kfIdArr
+		println System.SendCommand(kfId & "*TIME GET")
+	next
+End Sub
 
 Sub createCSV()
 	Dim filePath As String = GetParameterString("filePath")
@@ -72,5 +141,6 @@ Structure ChannelGRP
     VizId As Integer
     type, cont As String
 End Structure
+
 
 
